3554eb743817f448b47d11a62f66a95be8a6cd32
FakerDev-44 <gmc45190@gmail.com>
2026-06-24 11:05:18 -0400

    fix Importaciones de excepciones y status

--- app/routes/artworks.py
+++ app/routes/artworks.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from app.config.database import db
 from app.schemas.artwork import ArtworkCreate, ArtworkResponse
 
